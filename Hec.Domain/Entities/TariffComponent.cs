0f31d53c76ac4b65d9c37f8da8fbaf45eb576105
Putera Amirul <puteraamirulhaqim19@gmail.com>
2025-07-29 16:31:52 +0800

    haaa

--- Hec.Domain/Entities/TariffComponent.cs
+++ Hec.Domain/Entities/TariffComponent.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,21 +8,21 @@ namespace Hec.Entities
 {
     public class TariffComponent : Entity
     {
-        public string ComponentName { get; set; }  // Energy, AFA, Capacity, Network, etc.
-        public string ComponentType { get; set; }  // "RATE" or "FIXED"
-        public double Rate { get; set; }          // Rate per kWh (sen/kWh) or fixed amount (RM)
-        public string Unit { get; set; }          // "sen/kWh" or "RM/month"
-        public DateTime EffectiveFrom { get; set; }
-        public DateTime? EffectiveTo { get; set; }
-        public bool IsActive { get; set; }
-        public int Sequence { get; set; }
-        
-        // Special conditions
-        public int? MinConsumptionKwh { get; set; }  // Minimum consumption for this component
-        public int? MaxConsumptionKwh { get; set; }  // Maximum consumption for this component
-        public bool ExemptServiceTax { get; set; }   // Whether this component is exempt from service tax
-        public bool ExemptKWTBB { get; set; }        // Whether this component is exempt from KWTBB
-        
-        public string Description { get; set; }
+        public string ComponentName { get; set; }
+        public string ComponentType { get; set; } = "RATE";  // Default value
+        public double Rate { get; set; } = 0.0;              // Default value
+        public string Unit { get; set; } = "sen/kWh";        // Default value
+        public DateTime EffectiveFrom { get; set; } = DateTime.Now; // Default value
+        public DateTime? EffectiveTo { get; set; }           // Nullable
+        public bool IsActive { get; set; } = true;           // Default value
+        public int Sequence { get; set; } = 1;               // Default value
+
+        // Special conditions - all nullable
+        public int? MinConsumptionKwh { get; set; }
+        public int? MaxConsumptionKwh { get; set; }
+        public bool ExemptServiceTax { get; set; } = false;  // Default value
+        public bool ExemptKWTBB { get; set; } = false;       // Default value
+
+        public string Description { get; set; } = "";        // Default value
     }
 }
--- Hec.Domain/Entities/TariffConfiguration.cs
+++ Hec.Domain/Entities/TariffConfiguration.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,22 +8,22 @@ namespace Hec.Entities
 {
     public class TariffConfiguration : Entity
     {
-        public string ConfigurationName { get; set; }  // "OLD_TARIFF" or "NEW_TARIFF"
-        public DateTime EffectiveFrom { get; set; }
-        public DateTime? EffectiveTo { get; set; }
-        public bool IsActive { get; set; }
-        
-        // Tax configurations
-        public double ServiceTaxRate { get; set; }     // 8%
-        public int ServiceTaxThresholdKwh { get; set; } // 600 kWh
-        
-        public double KwtbbRate { get; set; }          // 1.6%
-        public int KwtbbThresholdKwh { get; set; }     // 300 kWh
-        
-        // Other thresholds
-        public int EnergyEfficiencyMaxKwh { get; set; } // 1000 kWh
-        public int RetailChargeThresholdKwh { get; set; } // 600 kWh
-        
-        public string Description { get; set; }
+        public string ConfigurationName { get; set; } = "";  // Default value
+        public DateTime EffectiveFrom { get; set; } = DateTime.Now; // Default value
+        public DateTime? EffectiveTo { get; set; }           // Nullable
+        public bool IsActive { get; set; } = true;           // Default value
+
+        // Tax configurations with defaults
+        public double ServiceTaxRate { get; set; } = 0.08;   // Default 8%
+        public int ServiceTaxThresholdKwh { get; set; } = 0; // Default 0
+
+        public double KwtbbRate { get; set; } = 0.016;       // Default 1.6%
+        public int KwtbbThresholdKwh { get; set; } = 0;      // Default 0
+
+        // Other thresholds with defaults
+        public int EnergyEfficiencyMaxKwh { get; set; } = 0; // Default 0
+        public int RetailChargeThresholdKwh { get; set; } = 0; // Default 0
+
+        public string Description { get; set; } = "";        // Default value
     }
 }
